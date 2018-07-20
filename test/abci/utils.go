327ee86253f0fcd52703c5bf9b15d7013a7e8dbf
danliu <danliu@localhost.localdomain>
2018-07-20 20:43:09 +0800

    Add more interfaces for test.

--- test/abci/utils.go
+++ test/abci/utils.go
@@ -32,15 +32,21 @@ func (tc *TestClient) DeliverTxAsync(msg sdk.Msg, cdc *wire.Codec) *abcicli.ReqR
 }
 
 func (tc *TestClient) CheckTxAsync(msg sdk.Msg, cdc *wire.Codec) *abcicli.ReqRes {
-	return nil
+	stdtx := auth.NewStdTx([]sdk.Msg{msg}, auth.NewStdFee(0), nil, "test")
+	tx, _ := tc.cdc.MarshalBinary(stdtx)
+	return tc.cl.CheckTxAsync(tx)
 }
 
 func (tc *TestClient) DeliverTxSync(msg sdk.Msg, cdc *wire.Codec) (*types.ResponseDeliverTx, error) {
-	return nil, nil
+	stdtx := auth.NewStdTx([]sdk.Msg{msg}, auth.NewStdFee(0), nil, "test")
+	tx, _ := tc.cdc.MarshalBinary(stdtx)
+	return tc.cl.DeliverTxSync(tx)
 }
 
 func (tc *TestClient) CheckTxSync(msg sdk.Msg, cdc *wire.Codec) (*types.ResponseCheckTx, error) {
-	return nil, nil
+	stdtx := auth.NewStdTx([]sdk.Msg{msg}, auth.NewStdFee(0), nil, "test")
+	tx, _ := tc.cdc.MarshalBinary(stdtx)
+	return tc.cl.CheckTxSync(tx)
 }
 
 // util objects
