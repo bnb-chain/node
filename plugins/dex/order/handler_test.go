3fd67720ca2fc7e2648192bd2b163951455fcaee
danliu <danliu@localhost.localdomain>
2018-07-22 18:09:18 +0800

    Add more skeleton about abci test.

--- plugins/dex/order/handler_test.go
+++ plugins/dex/order/handler_test.go
@@ -1,37 +1,31 @@
 package order
 
 // import (
-// 	"bytes"
-// 	"log"
 // 	"testing"
 
-// 	"github.com/BiJie/BinanceChain/common"
-// 	bnt "github.com/BiJie/BinanceChain/common/types"
-// 	"github.com/BiJie/BinanceChain/plugins/dex/store"
-// 	"github.com/BiJie/BinanceChain/plugins/dex/types"
-// 	tokenStore "github.com/BiJie/BinanceChain/plugins/tokens/store"
+// 	o "github.com/BiJie/BinanceChain/plugins/dex/order"
+// 	test "github.com/BiJie/BinanceChain/test/abci"
 // 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	"github.com/cosmos/cosmos-sdk/x/auth"
-// 	"github.com/cosmos/cosmos-sdk/x/bank"
 // 	"github.com/stretchr/testify/assert"
 // 	abci "github.com/tendermint/tendermint/abci/types"
 // )
 
 // func Test_handleNewOrder(t *testing.T) {
 // 	assert := assert.New(t)
-// 	var cdc = MakeCodec()
+// 	var cdc = test.TA().GetCodec()
 // 	// mappers
-// 	accountMapper := auth.NewAccountMapper(cdc, common.AccountStoreKey, bnt.ProtoAppAccount)
-// 	tokenMapper := tokenStore.NewMapper(cdc, common.TokenStoreKey)
-// 	tradingPairMapper := store.NewTradingPairMapper(cdc, common.PairStoreKey)
+// 	accountMapper := test.TA().GetAccountMapper()
+// 	tokenMapper := test.TA().GetTokenMapper()
 
 // 	// Add handlers.
-// 	coinKeeper := bank.NewKeeper(app.accountMapper)
+// 	coinKeeper := test.TA().GetCoinKeeper()
 // 	// TODO: make the concurrency configurable
-// 	orderKeeper := NewOrderKeeper(common.DexStoreKey, app.coinKeeper, app.RegisterCodespace(types.DefaultCodespace), 2)
+// 	orderKeeper := test.TA().GetOrderKeeper()
 
-// 	var buf bytes.Buffer
-// 	logger := log.New(&buf, "logger: ", log.Lshortfile)
-// 	ctx := sdk.NewContext(memStore, abci.Header{ChainID: "1", Height: 1}, true, logger)
+// 	ctx := test.TA().NewContext(true, abci.Header{})
+// 	acct, _ := sdk.AccAddressFromHex("bc1handlenew0rder2")
+// 	msg := o.NewOrderMsg(acct, "Reject1", "BTC_BNB", 2, 1, 100, 200, 1)
 
+// 	res := o.handleNewOrder(ctx, orderKeeper, accountMapper, msg)
+// 	t.Log(res)
 // }
