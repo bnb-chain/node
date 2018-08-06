2979b950d2680a9c3b7ffc98fdd53451096196d0
yutianwu <wzxingbupt@gmail.com>
2018-08-06 17:26:35 +0800

    add async file writer

---
+++ common/log/async_file_writer_test.go
@@ -0,0 +1,14 @@
+package log
+
+import (
+	"testing"
+)
+
+func TestWriter(t *testing.T) {
+	w := NewAsyncFileWriter("./hello.log", 100)
+	w.InitLogFile()
+	w.Start()
+	w.Write([]byte("hello\n"))
+	w.Write([]byte("world\n"))
+	w.Stop()
+}
