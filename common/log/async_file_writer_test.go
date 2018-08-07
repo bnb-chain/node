38014cdf30a2247e90c539c6b486e1cb1d753eac
yutianwu <wzxingbupt@gmail.com>
2018-08-07 11:33:02 +0800

    do some refactor

--- common/log/async_file_writer_test.go
+++ common/log/async_file_writer_test.go
@@ -2,13 +2,14 @@ package log
 
 import (
 	"testing"
+	"time"
 )
 
 func TestWriter(t *testing.T) {
 	w := NewAsyncFileWriter("./hello.log", 100)
-	w.InitLogFile()
 	w.Start()
 	w.Write([]byte("hello\n"))
 	w.Write([]byte("world\n"))
 	w.Stop()
+	time.Sleep(10 * time.Second)
 }
