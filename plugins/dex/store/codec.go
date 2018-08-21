4904c6b3e01b631ff639eae0f9c020c13e57039d
Luke Plaster <me@lukep.org>
2018-08-21 15:06:23 +0800

    dex api: streaming depth api following cex format

--- plugins/dex/store/codec.go
+++ plugins/dex/store/codec.go
@@ -8,27 +8,16 @@ import (
 	"github.com/BiJie/BinanceChain/wire"
 )
 
-// queryOrderBook queries the store for the serialized order book for a given pair.
-func queryOrderBook(cdc *wire.Codec, ctx context.CoreContext, pair string) (*[]byte, error) {
+func queryOrderBook(cdc *wire.Codec, ctx context.CoreContext, pair string) ([]byte, error) {
 	bz, err := ctx.Query(fmt.Sprintf("app/orderbook/%s", pair))
 	if err != nil {
 		return nil, err
 	}
-	return &bz, nil
+	return bz, nil
 }
 
-// decodeOrderBook decodes the order book to a set of OrderBookLevel structs
-func decodeOrderBook(cdc *wire.Codec, bz *[]byte) (*[]OrderBookLevel, error) {
-	levels := make([]OrderBookLevel, 0)
-	err := cdc.UnmarshalBinary(*bz, &levels)
-	if err != nil {
-		return nil, err
-	}
-	return &levels, nil
-}
-
-// GetOrderBook decodes the order book from the serialized store
-func GetOrderBook(cdc *wire.Codec, ctx context.CoreContext, pair string) (*[]OrderBookLevel, error) {
+// GetOrderBook decodes the order book from the store
+func GetOrderBook(cdc *wire.Codec, ctx context.CoreContext, pair string) (*[]Order, error) {
 	bz, err := queryOrderBook(cdc, ctx, pair)
 	if err != nil {
 		return nil, err
@@ -36,6 +25,49 @@ func GetOrderBook(cdc *wire.Codec, ctx context.CoreContext, pair string) (*[]Ord
 	if bz == nil {
 		return nil, nil
 	}
+	book, err := DecodeOrderBook(cdc, &bz)
+	return book, err
+}
+
+// GetOrderBookRaw decodes the raw order book from the store
+func GetOrderBookRaw(cdc *wire.Codec, ctx context.CoreContext, pair string) (*[][]int64, error) {
+	bz, err := queryOrderBook(cdc, ctx, pair)
+	if err != nil {
+		return nil, err
+	}
+	if bz == nil {
+		return nil, nil
+	}
+	table, err := DecodeOrderBookRaw(cdc, &bz)
+	return table, err
+}
+
+// DecodeOrderBook decodes the order book to a set of Order structs
+func DecodeOrderBook(cdc *wire.Codec, bz *[]byte) (*[]Order, error) {
+	table, err := DecodeOrderBookRaw(cdc, bz)
+	if err != nil {
+		return nil, err
+	}
+	book := make([]Order, 0)
+	for _, o := range *table {
+		order := Order{
+			SellQty:   utils.Fixed8(o[0]),
+			SellPrice: utils.Fixed8(o[1]),
+			BuyPrice:  utils.Fixed8(o[2]),
+			BuyQty:    utils.Fixed8(o[3]),
+		}
+		book = append(book, order)
+	}
 	book, err := decodeOrderBook(cdc, bz)
 	return book, err
+}
+
+// DecodeOrderBookRaw decodes the raw order book table
+func DecodeOrderBookRaw(cdc *wire.Codec, bz *[]byte) (*[][]int64, error) {
+	table := make([][]int64, 0)
+	err := cdc.UnmarshalBinary(*bz, &table)
+	if err != nil {
+		return nil, err
+	}
+	return &table, nil
 }
