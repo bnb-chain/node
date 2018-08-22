c408b4c0eb3fc7e10952430c38a2f2ad2f0db9fa
Luke Plaster <me@lukep.org>
2018-08-22 14:38:53 +0800

    dex api: depth symbol and limit are query params

--- plugins/api/routes.go
+++ plugins/api/routes.go
@@ -15,16 +15,26 @@ func (s *server) bindRoutes() *server {
 	r := s.router
 
 	// version routes
-	r.HandleFunc("/version", s.handleVersionReq()).Methods("GET")
-	r.HandleFunc("/node_version", s.handleNodeVersionReq()).Methods("GET")
+	r.HandleFunc("/version", s.handleVersionReq()).
+		Methods("GET")
+	r.HandleFunc("/node_version", s.handleNodeVersionReq()).
+		Methods("GET")
 
 	// dex routes
-	r.HandleFunc(prefix+"/order", s.handleDexOrderReq(s.cdc, s.ctx, s.accStoreName)).Methods("PUT", "POST")
-	r.HandleFunc(prefix+"/depth/{pair}", s.handleDexDepthReq(s.cdc, s.ctx)).Methods("GET")
+	r.HandleFunc(prefix+"/order", s.handleDexOrderReq(s.cdc, s.ctx, s.accStoreName)).
+		Methods("PUT", "POST")
+	r.HandleFunc(prefix+"/depth", s.handleDexDepthReq(s.cdc, s.ctx)).
+		Queries("symbol", "{symbol}", "limit", "{limit:[0-9]+}").
+		Methods("GET")
+	r.HandleFunc(prefix+"/depth", s.handleDexDepthReq(s.cdc, s.ctx)).
+		Queries("symbol", "{symbol}").
+		Methods("GET")
 
 	// tokens routes
-	r.HandleFunc(prefix+"/balances/{address}", s.handleBalancesReq(s.cdc, s.ctx, s.tokens)).Methods("GET")
-	r.HandleFunc(prefix+"/balances/{address}/{symbol}", s.handleBalanceReq(s.cdc, s.ctx, s.tokens)).Methods("GET")
+	r.HandleFunc(prefix+"/balances/{address}", s.handleBalancesReq(s.cdc, s.ctx, s.tokens)).
+		Methods("GET")
+	r.HandleFunc(prefix+"/balances/{address}/{symbol}", s.handleBalanceReq(s.cdc, s.ctx, s.tokens)).
+		Methods("GET")
 
 	// legacy plugin routes
 	// TODO: make these more like the above for simplicity.
--- plugins/dex/client/rest/getdepth.go
+++ plugins/dex/client/rest/getdepth.go
@@ -1,9 +1,9 @@
 package rest
 
 import (
+	"errors"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 
@@ -12,10 +12,13 @@ import (
 	"github.com/BiJie/BinanceChain/wire"
 )
 
+const maxUint = ^uint(0)
+
 // DepthReqHandler creates an http request handler to send coins to a address
 func DepthReqHandler(cdc *wire.Codec, ctx context.CoreContext) http.HandlerFunc {
 	type params struct {
-		pair string
+		symbol string
+		limit  int
 	}
 	throw := func(w http.ResponseWriter, status int, err error) {
 		w.WriteHeader(status)
@@ -23,27 +26,37 @@ func DepthReqHandler(cdc *wire.Codec, ctx context.CoreContext) http.HandlerFunc 
 		return
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-
 		// collect params
+		limitStr := r.FormValue("limit")
+		limit := int(maxUint >> 1)
+		if len(limitStr) > 0 {
+			var err error
+			limit, err = strconv.Atoi(limitStr)
+			if err != nil {
+				throw(w, http.StatusExpectationFailed, errors.New("invalid limit"))
+				return
+			}
+		}
+
 		params := params{
-			pair: vars["pair"],
+			symbol: r.FormValue("symbol"),
+			limit:  limit,
 		}
 
 		// validate pair
-		err := store.ValidatePairSymbol(params.pair)
+		err := store.ValidatePairSymbol(params.symbol)
 		if err != nil {
 			throw(w, http.StatusNotFound, err)
 			return
 		}
 
-		table, err := store.GetOrderBook(cdc, ctx, params.pair)
+		table, err := store.GetOrderBook(cdc, ctx, params.symbol)
 		if err != nil {
 			throw(w, http.StatusNotFound, err)
 			return
 		}
 
-		err = rutils.StreamDepthResponse(w, table)
+		err = rutils.StreamDepthResponse(w, table, limit)
 		if err != nil {
 			throw(w, http.StatusInternalServerError, err)
 			return
