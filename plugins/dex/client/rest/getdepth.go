b8412c330d87186968f6992b287c054bf73d9abc
Luke Plaster <me@lukep.org>
2018-08-24 12:35:37 +0800

    dex api: whitelist for depth limit params
    currently supported: 5,10,20,50,100

--- plugins/dex/client/rest/getdepth.go
+++ plugins/dex/client/rest/getdepth.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 
@@ -12,10 +13,13 @@ import (
 	"github.com/BiJie/BinanceChain/wire"
 )
 
+const allowedLimits = "5,10,20,50,100"
+const defaultLimit = "100"
 const maxUint = ^uint(0)
 
 // DepthReqHandler creates an http request handler to send coins to a address
 func DepthReqHandler(cdc *wire.Codec, ctx context.CoreContext) http.HandlerFunc {
+	allowedLimitsA := strings.Split(allowedLimits, ",")
 	type params struct {
 		symbol string
 		limit  int
@@ -28,10 +32,20 @@ func DepthReqHandler(cdc *wire.Codec, ctx context.CoreContext) http.HandlerFunc 
 	return func(w http.ResponseWriter, r *http.Request) {
 		// collect params
 		limitStr := r.FormValue("limit")
+
+		// validate limit param
+		limitStrOk := defaultLimit
+		for _, lmt := range allowedLimitsA {
+			if lmt == limitStr {
+				limitStrOk = limitStr
+				break
+			}
+		}
+
 		limit := int(maxUint >> 1)
-		if len(limitStr) > 0 {
+		if len(limitStrOk) > 0 {
 			var err error
-			limit, err = strconv.Atoi(limitStr)
+			limit, err = strconv.Atoi(limitStrOk)
 			if err != nil {
 				throw(w, http.StatusExpectationFailed, errors.New("invalid limit"))
 				return
