873cf1524cfd3d9b020b18473cc04ed7a818d9a4
Luke Plaster <me@lukep.org>
2018-09-06 16:32:48 +0800

    Restructured plugin init and abci query handler mgmt

--- plugins/dex/plugin.go
+++ plugins/dex/plugin.go
@@ -3,8 +3,23 @@ package dex
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+
+	app "github.com/BiJie/BinanceChain/common/types"
 )
 
+const abciQueryPrefix = "dex"
+
+// InitPlugin initializes the dex plugin.
+func InitPlugin(appp app.ChainApp, keeper *DexKeeper) {
+	handler := createQueryHandler(keeper)
+	appp.RegisterQueryHandler(abciQueryPrefix, handler)
+}
+
+func createQueryHandler(keeper *DexKeeper) app.AbciQueryHandler {
+	return createAbciQueryHandler(keeper)
+}
+
+// EndBreatheBlock processes the breathe block lifecycle event.
 func EndBreatheBlock(ctx sdk.Context, accountMapper auth.AccountMapper, dexKeeper DexKeeper, height, blockTime int64) {
 	updateTickSizeAndLotSize(ctx, dexKeeper)
 	dexKeeper.ExpireOrders(ctx, height, accountMapper)
