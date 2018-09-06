f646d4c3ae7a0d57fbd0315b9354ef5b74f8ebfe
Luke Plaster <me@lukep.org>
2018-09-06 16:32:48 +0800

    Restructured plugin init and abci query handler mgmt

---
+++ common/types/app.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"encoding/json"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmtypes "github.com/tendermint/tendermint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/BiJie/BinanceChain/wire"
+)
+
+// ChainApp represents the main ABCI application
+type ChainApp interface {
+	GetCodec() *wire.Codec
+	Query(req abci.RequestQuery) (res abci.ResponseQuery)
+	RegisterQueryHandler(prefix string, handler AbciQueryHandler)
+	ExportAppStateAndValidators() (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error)
+	EndBlocker(ctx sdk.Context, req abci.RequestEndBlock) abci.ResponseEndBlock
+}
---
+++ plugins/dex/abci.go
@@ -0,0 +1,51 @@
+package dex
+
+import (
+	"fmt"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	app "github.com/BiJie/BinanceChain/common/types"
+)
+
+func createAbciQueryHandler(keeper *DexKeeper) app.AbciQueryHandler {
+	return func(app app.ChainApp, req abci.RequestQuery, path []string) (res *abci.ResponseQuery) {
+		// expects at least two query path segments.
+		if path[0] != abciQueryPrefix || len(path) < 2 {
+			return nil
+		}
+		switch path[1] {
+		case "pairs":
+			keeper.PairMapper
+		case "orderbook":
+			//TODO: sync lock, validate pair, level number
+			if len(path) < 3 {
+				return &abci.ResponseQuery{
+					Code: uint32(sdk.CodeUnknownRequest),
+					Log:  "OrderBook query requires the pair symbol",
+				}
+			}
+			pair := path[2]
+			orderbook := keeper.GetOrderBook(pair, 20)
+			resValue, err := app.GetCodec().MarshalBinary(orderbook)
+			if err != nil {
+				return &abci.ResponseQuery{
+					Code: uint32(sdk.CodeInternal),
+					Log:  err.Error(),
+				}
+			}
+
+			return &abci.ResponseQuery{
+				Code:  uint32(sdk.ABCICodeOK),
+				Value: resValue,
+			}
+		default:
+			return &abci.ResponseQuery{
+				Code: uint32(sdk.ABCICodeOK),
+				Info: fmt.Sprintf("Unknown `%s` query path", abciQueryPrefix),
+			}
+		}
+	}
+}
