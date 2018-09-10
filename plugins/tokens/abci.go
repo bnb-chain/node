869ec1572e4114862020b7e76633f933537d9ee4
Luke Plaster <me@lukep.org>
2018-09-10 17:39:32 +0800

    dex/tokens abci: generic malformed path logging

--- plugins/tokens/abci.go
+++ plugins/tokens/abci.go
@@ -22,7 +22,7 @@ func createAbciQueryHandler(mapper Mapper) app.AbciQueryHandler {
 			if len(path) < 4 {
 				return &abci.ResponseQuery{
 					Code: uint32(sdk.CodeUnknownRequest),
-					Log:  "tokens list query requires offset and limit in the path",
+					Log:  fmt.Sprintf("%s %s query requires offset and limit in the path", abciQueryPrefix, path[1]),
 				}
 			}
 			ctx := app.GetContextForCheckState()
