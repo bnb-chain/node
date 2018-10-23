1580a3edb0b833361491051202bb2686ffb72e7b
Luke Plaster <me@lukep.org>
2018-10-23 10:07:42 +0800

    Revert "Clean up: Msg handlers moved to InitPlugin funcs (#165)"
    This reverts commit 86138478fd1b2a29e42e187f365a45ea1ef98756.

--- plugins/tokens/plugin.go
+++ plugins/tokens/plugin.go
@@ -1,24 +1,13 @@
 package tokens
 
 import (
-	"github.com/cosmos/cosmos-sdk/x/auth"
-	"github.com/cosmos/cosmos-sdk/x/bank"
-
 	app "github.com/BiJie/BinanceChain/common/types"
 )
 
 const abciQueryPrefix = "tokens"
 
 // InitPlugin initializes the plugin.
-func InitPlugin(
-	appp app.ChainApp, mapper Mapper, accMapper auth.AccountMapper, coinKeeper bank.Keeper,
-) {
-	// add msg handlers
-	for route, handler := range Routes(mapper, accMapper, coinKeeper) {
-		appp.GetRouter().AddRoute(route, handler)
-	}
-
-	// add abci handlers
+func InitPlugin(appp app.ChainApp, mapper Mapper) {
 	handler := createQueryHandler(mapper)
 	appp.RegisterQueryHandler(abciQueryPrefix, handler)
 }
