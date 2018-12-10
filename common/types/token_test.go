c5f41012a83ecef2fa57db0df88347408f0220be
Luke Plaster <me@lukep.org>
2018-12-10 18:28:18 +0800

    types/token_test: removed duplicate test case

--- common/types/token_test.go
+++ common/types/token_test.go
@@ -39,7 +39,6 @@ var tokenMapperSymbolTestCases = []struct {
 	{"XYZ-000", true},
 	{"1YZ-000", true},
 	{"XYZ.B-000", true},
-	{"XYZ.B-000", true},
 	// sad
 	{types.NativeTokenSymbol+"-000", false}, // no tx hash suffix for native token
 	{types.NativeTokenSymbolDotBSuffixed+"-000", false},
