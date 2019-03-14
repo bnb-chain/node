0e66eecbe6670385643dbfd5d80e5c644c307e24
fudongbai <296179868@qq.com>
2019-03-14 19:16:17 +0800

    Field: price of TradingPair change to list_price

--- plugins/dex/types/pair.go
+++ plugins/dex/types/pair.go
@@ -8,13 +8,13 @@ import (
 type TradingPair struct {
 	BaseAssetSymbol  string        `json:"base_asset_symbol"`
 	QuoteAssetSymbol string        `json:"quote_asset_symbol"`
-	Price            ctuils.Fixed8 `json:"price"`
+	ListPrice        ctuils.Fixed8 `json:"list_price"`
 	TickSize         ctuils.Fixed8 `json:"tick_size"`
 	LotSize          ctuils.Fixed8 `json:"lot_size"`
 }
 
-func NewTradingPair(baseAssetSymbol, quoteAssetSymbol string, price int64) TradingPair {
-	tickSize, lotSize := utils.CalcTickSizeAndLotSize(price)
+func NewTradingPair(baseAssetSymbol, quoteAssetSymbol string, listPrice int64) TradingPair {
+	tickSize, lotSize := utils.CalcTickSizeAndLotSize(listPrice)
 
 	// TODO: symbol validations should also happen here, but a lot of tests rely on this method.
 	//       for now, these checks are done in TradingPairMapper#AddTradingPair.
@@ -22,7 +22,7 @@ func NewTradingPair(baseAssetSymbol, quoteAssetSymbol string, price int64) Tradi
 	return TradingPair{
 		BaseAssetSymbol:  baseAssetSymbol,
 		QuoteAssetSymbol: quoteAssetSymbol,
-		Price:            ctuils.Fixed8(price),
+		ListPrice:        ctuils.Fixed8(listPrice),
 		TickSize:         ctuils.Fixed8(tickSize),
 		LotSize:          ctuils.Fixed8(lotSize),
 	}
