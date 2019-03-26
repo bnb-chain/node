b01a4402781c1c91aea4258c46228fb658836dad
rickyyangz <yangzhongzhong@ruiquewenhua.com>
2019-03-26 15:09:25 +0800

    further fixes about overflow and float64

--- common/utils/numbers.go
+++ common/utils/numbers.go
@@ -28,3 +28,16 @@ func Int642Bytes(n int64) []byte {
 	binary.LittleEndian.PutUint64(b, uint64(n))
 	return b
 }
+
+func Mul64(a, b int64) (int64, bool) {
+	if a == 0 || b == 0 {
+		return 0, true
+	}
+	c := a * b
+	if (c < 0) == ((a < 0) != (b < 0)) {
+		if c/b == a {
+			return c, true
+		}
+	}
+	return c, false
+}
