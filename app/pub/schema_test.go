412407fa019864e22ffab8e75dd61f76e9419b9a
HaoyangLiu <lhy.0318@163.com>
2019-04-11 16:55:25 +0800

    [R4R]: publish stake endblock updated accounts (#519)
    * publish stake endblock updated accounts
    
    * remove unexpected change
    
    * Fix some bugs in collect stake updates
    
    * refactor package import
    
    * Refactor variables name
    
    * Change cosmos dependency to develop branch
    
    * Resolve comment, add schema and unit test
    
    * Remove todo
    
    * Correct total msg quantity

--- app/pub/schema_test.go
+++ app/pub/schema_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/binance-chain/node/app/config"
 	"github.com/binance-chain/node/common/log"
 	orderPkg "github.com/binance-chain/node/plugins/dex/order"
@@ -43,13 +45,27 @@ func TestExecutionResultsMarshaling(t *testing.T) {
 			{3, Failed},
 		},
 	}
+
+	valAddr, _ :=sdk.ValAddressFromBech32("bva1e2y8w2rz957lahwy0y5h3w53sm8d78qexkn3rh")
+	delAddr, _ :=sdk.AccAddressFromBech32("bnb1e2y8w2rz957lahwy0y5h3w53sm8d78qex2jpan")
+	stakeUpdates := StakeUpdates{
+		NumOfMsgs: 1,
+		CompletedUnbondingDelegations: []*CompletedUnbondingDelegation{
+			{
+				Validator: valAddr,
+				Delegator: delAddr,
+				Amount: Coin{"BNB", 100000000000},
+			},
+		},
+	}
 	msg := ExecutionResults{
 		Height:    42,
 		Timestamp: 100,
-		NumOfMsgs: 4,
+		NumOfMsgs: 8,
 		Trades:    trades,
 		Orders:    orders,
 		Proposals: proposals,
+		StakeUpdates: stakeUpdates,
 	}
 	_, err := publisher.marshal(&msg, executionResultTpe)
 	if err != nil {
