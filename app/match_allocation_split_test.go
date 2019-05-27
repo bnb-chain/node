ea390b135812490bc6fd0bea73d35031601d71e9
Xiang Li <sandylx@hotmail.com>
2019-05-27 16:44:55 +0800

    add test case for fixes for match engine #584

--- app/match_allocation_split_test.go
+++ app/match_allocation_split_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -278,4 +279,120 @@ func Test_Split_2(t *testing.T) {
 	assert.Equal(int64(99990e8), GetAvail(ctx, addr3, "BTC-000"))
 	assert.Equal(int64(100019.99e8), GetAvail(ctx, addr3, "BNB"))
 	assert.Equal(int64(0), GetLocked(ctx, addr3, "BTC-000"))
+}
+
+// lot size changed (e.g. due to significant price changes) lead to not enough balance, chain panic
+func Test_Split_3(t *testing.T) {
+	assert := assert.New(t)
+
+	addr, ctx, accs := SetupTest(1e8)
+	addr0 := accs[0].GetAddress()
+	addr1 := accs[1].GetAddress()
+	addr2 := accs[2].GetAddress()
+	addr3 := accs[3].GetAddress()
+	// the original lotsize is 1e5
+
+	ctx = UpdateContext(addr, ctx, 1)
+
+	oidS1 := GetOrderId(addr0, 0, ctx)
+	msgS1 := order.NewNewOrderMsg(addr0, oidS1, 2, "BTC-000_BNB", 1e4, 2e8)
+	res, err := testClient.DeliverTxSync(msgS1, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	oidS2 := GetOrderId(addr1, 0, ctx)
+	msgS2 := order.NewNewOrderMsg(addr1, oidS2, 2, "BTC-000_BNB", 1e4, 20e8)
+	res, err = testClient.DeliverTxSync(msgS2, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	oidS3 := GetOrderId(addr2, 0, ctx)
+	msgS3 := order.NewNewOrderMsg(addr2, oidS3, 2, "BTC-000_BNB", 1e4, 12e8)
+	res, err = testClient.DeliverTxSync(msgS3, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	oidB1 := GetOrderId(addr3, 0, ctx)
+	msgB1 := order.NewNewOrderMsg(addr3, oidB1, 1, "BTC-000_BNB", 1e4, 10e8)
+	res, err = testClient.DeliverTxSync(msgB1, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	buys, sells := GetOrderBook("BTC-000_BNB")
+	assert.Equal(utils.Fixed8(32e8), buys[0].qty)
+	assert.Equal(utils.Fixed8(10e8), sells[0].qty)
+
+	testApp.DexKeeper.UpdateLotSize("BTC-000_BNB", 1e9)
+
+	testClient.cl.EndBlockSync(abci.RequestEndBlock{})
+
+	// panic: no enough locked tokens to unlock,
+	// oid: 7E4FF4423F1CDF142E915A7A08247381280E78DB-0,
+	// newLocked: -800000000BTC-000, unlock: 1000000000
+}
+
+func Test_Split_4(t *testing.T) {
+	assert := assert.New(t)
+
+	addr, ctx, accs := SetupTest(10)
+	addr0 := accs[0].GetAddress()
+	addr1 := accs[1].GetAddress()
+	addr2 := accs[2].GetAddress()
+	addr3 := accs[3].GetAddress()
+
+	ctx = UpdateContext(addr, ctx, 1)
+
+	oidB := GetOrderId(addr0, 0, ctx)
+	msgB := order.NewNewOrderMsg(addr0, oidB, 1, "BTC-000_BNB", 1, 1e12)
+	res, err := testClient.DeliverTxSync(msgB, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	oidS1 := GetOrderId(addr1, 0, ctx)
+	msgS1 := order.NewNewOrderMsg(addr1, oidS1, 2, "BTC-000_BNB", 1, 1e12)
+	res, err = testClient.DeliverTxSync(msgS1, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	oidS3 := GetOrderId(addr2, 0, ctx)
+	msgS3 := order.NewNewOrderMsg(addr2, oidS3, 2, "BTC-000_BNB", 1, 1e12)
+	res, err = testClient.DeliverTxSync(msgS3, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	oidS4 := GetOrderId(addr3, 0, ctx)
+	msgS4 := order.NewNewOrderMsg(addr3, oidS4, 2, "BTC-000_BNB", 1, 1e12)
+	res, err = testClient.DeliverTxSync(msgS4, testApp.Codec)
+	assert.NoError(err)
+	fmt.Println(res)
+
+	buys, sells := GetOrderBook("BTC-000_BNB")
+	assert.Equal(1, len(buys))
+	assert.Equal(utils.Fixed8(3e12), sells[0].qty)
+
+	testClient.cl.EndBlockSync(abci.RequestEndBlock{})
+
+	trades, lastPx := testApp.DexKeeper.GetLastTradesForPair("BTC-000_BNB")
+	assert.Equal(int64(1), lastPx)
+	assert.Equal(1, len(trades))
+
+	buys, sells = GetOrderBook("BTC-000_BNB")
+	assert.Equal(0, len(buys))
+	assert.Equal(utils.Fixed8(2e12), sells[0].qty)
+
+	assert.Equal(int64(11000000000000), GetAvail(ctx, addr0, "BTC-000"))
+	assert.Equal(int64(9999999989995), GetAvail(ctx, addr0, "BNB"))
+	assert.Equal(int64(0), GetLocked(ctx, addr0, "BNB"))
+
+	assert.Equal(int64(9000000000000), GetAvail(ctx, addr1, "BTC-000"))
+	assert.Equal(int64(10000000009995), GetAvail(ctx, addr1, "BNB"))
+	assert.Equal(int64(0), GetLocked(ctx, addr1, "BTC-000"))
+
+	assert.Equal(int64(9000000000000), GetAvail(ctx, addr2, "BTC-000"))
+	assert.Equal(int64(10000000000000), GetAvail(ctx, addr2, "BNB"))
+	assert.Equal(int64(1000000000000), GetLocked(ctx, addr2, "BTC-000"))
+
+	assert.Equal(int64(9000000000000), GetAvail(ctx, addr3, "BTC-000"))
+	assert.Equal(int64(10000000000000), GetAvail(ctx, addr3, "BNB"))
+	assert.Equal(int64(1000000000000), GetLocked(ctx, addr3, "BTC-000"))
 }
