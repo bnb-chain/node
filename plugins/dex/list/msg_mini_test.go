f099e1f29bf614336ebe002e642ccae86b32c63d
erhenglu <42333959+erhenglu@users.noreply.github.com>
2020-06-30 23:07:41 +0800

    Force match all BEP2 symbols on BEP8 upgrade height to update last ma… (#758)
     Force match all BEP2 symbols on BEP8 upgrade height to update last match height

--- plugins/dex/list/msg_mini_test.go
+++ plugins/dex/list/msg_mini_test.go
@@ -7,61 +7,35 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/binance-chain/node/plugins/dex/order"
+	dextypes "github.com/binance-chain/node/plugins/dex/types"
 )
 
-func TestMiniIdenticalBaseAssetAndQuoteAsset(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BTC-000M", 1000)
-	err := msg.ValidateBasic()
-	require.NotNil(t, err, "msg should be error")
-	require.Contains(t, err.Error(), "quote token is not valid")
-}
 
 func TestMiniWrongBaseAssetSymbol(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC", "BTC-000", 1000)
+	msg := dextypes.NewListMiniMsg(sdk.AccAddress{}, "BTC", "BTC-000", 1000)
 	err := msg.ValidateBasic()
 	require.NotNil(t, err, "msg should be error")
 	require.Contains(t, err.Error(), "base token: suffixed token symbol must contain a hyphen ('-')")
 }
 
 func TestMiniWrongBaseAssetSymbolNotMiniToken(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC-000", "BTC-000", 1000)
+	msg := dextypes.NewListMiniMsg(sdk.AccAddress{}, "BTC-000", "BTC-000", 1000)
 	err := msg.ValidateBasic()
 	require.NotNil(t, err, "msg should be error")
 	require.Contains(t, err.Error(), "base token: mini-token symbol suffix must be 4 chars in length, got 3")
 }
 
-func TestMiniWrongQuoteAssetSymbol(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "ETH-123", 1000)
-	err := msg.ValidateBasic()
-	require.NotNil(t, err, "msg should be error")
-	require.Contains(t, err.Error(), "quote token is not valid")
-}
 
 func TestMiniWrongInitPrice(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BNB", -1000)
+	msg := dextypes.NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BNB", -1000)
 	err := msg.ValidateBasic()
 	require.NotNil(t, err, "msg should be error")
 	require.Contains(t, err.Error(), "price should be positive")
 }
 
 func TestMiniRightMsg(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BNB", 1000)
+	msg := dextypes.NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BNB", 1000)
 	err := msg.ValidateBasic()
 	require.Nil(t, err, "msg should not be error")
 }
 
-func TestMiniBUSDQuote(t *testing.T) {
-	msg := NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BUSD-000", 1000)
-	err := msg.ValidateBasic()
-	require.NotNil(t, err, "msg should be error")
-	require.Contains(t, err.Error(), "quote token is not valid")
-
-	setChainVersion()
-	defer resetChainVersion()
-	order.BUSDSymbol = "BUSD-000"
-	msg = NewListMiniMsg(sdk.AccAddress{}, "BTC-000M", "BUSD-000", 1000)
-	err = msg.ValidateBasic()
-	require.Nil(t, err, "msg should not be error")
-
-}
--- plugins/param/genesis.go
+++ plugins/param/genesis.go
@@ -6,8 +6,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/stake"
 
 	sdk "github.com/binance-chain/node/common/types"
-	"github.com/binance-chain/node/plugins/dex/list"
 	"github.com/binance-chain/node/plugins/dex/order"
+	"github.com/binance-chain/node/plugins/dex/types"
 	param "github.com/binance-chain/node/plugins/param/types"
 	"github.com/binance-chain/node/plugins/tokens/burn"
 	"github.com/binance-chain/node/plugins/tokens/freeze"
@@ -74,7 +74,7 @@ var FeeGenesisState = []param.FeeParam{
 	&param.FixedFeeParams{gov.MsgVote{}.Type(), sdk.ZeroFee, sdk.FeeFree},
 	&param.FixedFeeParams{stake.MsgCreateValidator{}.Type(), CreateValidatorFee, sdk.FeeForProposer},
 	&param.FixedFeeParams{stake.MsgRemoveValidator{}.Type(), RemoveValidatorFee, sdk.FeeForProposer},
-	&param.FixedFeeParams{list.Route, ListingFee, sdk.FeeForAll},
+	&param.FixedFeeParams{types.ListRoute, ListingFee, sdk.FeeForAll},
 	&param.FixedFeeParams{order.RouteNewOrder, sdk.ZeroFee, sdk.FeeFree},
 	&param.FixedFeeParams{order.RouteCancelOrder, sdk.ZeroFee, sdk.FeeFree},
 	&param.FixedFeeParams{issue.IssueMsgType, IssueFee, sdk.FeeForAll},
