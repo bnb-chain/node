9237bb022f1256a9cdd9fe0fecda2a986981fec7
WayToFuture <61674316+forcodedancing@users.noreply.github.com>
2022-03-08 16:50:49 +0800

    [R4R] sidechain staking reward distribution (#835)
    * sidechain staking reward distribution

--- common/upgrade/upgrade.go
+++ common/upgrade/upgrade.go
@@ -23,7 +23,6 @@ const (
 	ListingRuleUpgrade   = "ListingRuleUpgrade" // Remove restriction that only the owner of base asset can list trading pair
 	FixZeroBalance       = "FixZeroBalance"
 
-	// TODO: add upgrade name
 	LaunchBscUpgrade = sdk.LaunchBscUpgrade
 
 	EnableAccountScriptsForCrossChainTransfer = "EnableAccountScriptsForCrossChainTransfer"
@@ -31,12 +30,14 @@ const (
 	//Nightingale upgrade
 	BEP8  = sdk.BEP8 // https://github.com/binance-chain/BEPs/pull/69 Mini token upgrade
 	BEP67 = "BEP67"  // https://github.com/binance-chain/BEPs/pull/67 Expiry time upgrade
-	BEP70 = "BEP70"  // https://github.com/binance-chain/BEPs/pull/70 BUSD Pair Upgrade
+	BEP70 = "BEP70"  // https://github.com/binance-chain/BEPs/pull/70 BUSD pair upgrade
 
 	BEP82             = sdk.BEP82 // https://github.com/binance-chain/BEPs/pull/82
-	BEP84             = "BEP84"   // https://github.com/binance-chain/BEPs/pull/84 Mirror Sync Upgrade
+	BEP84             = "BEP84"   // https://github.com/binance-chain/BEPs/pull/84 Mirror sync upgrade
 	BEP87             = "BEP87"   // https://github.com/binance-chain/BEPs/pull/87
 	FixFailAckPackage = sdk.FixFailAckPackage
+
+	BEP128 = sdk.BEP128 // https://github.com/binance-chain/BEPs/pull/128 Staking reward distribution upgrade
 )
 
 func UpgradeBEP10(before func(), after func()) {
