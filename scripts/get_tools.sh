efa5369d461ef345d0de4a3caf6b29525daf8ce5
Owen Hu <owen.h@nodereal.io>
2022-04-21 22:22:43 +0800

    fix make test

--- scripts/get_tools.sh
+++ scripts/get_tools.sh
@@ -44,5 +44,4 @@ set -e
 #installFromGithub sasha-s/go-deadlock d68e2bc52ae3291765881b9056f2c1527f245f1e
 go get github.com/petermattis/goid@b0b1615b78e5ee59739545bb38426383b2cda4c9
 go get github.com/sasha-s/go-deadlock@d68e2bc52ae3291765881b9056f2c1527f245f1e
-go mod download golang.org/x/tools
-go install golang.org/x/tools/cmd/goimports
+go install golang.org/x/tools/cmd/goimports@latest
