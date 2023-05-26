163e87728c8935dc1204d91eee14a7daf03dd921
forcodedancing <just.haha.it@gmail.com>
2023-05-26 14:25:18 +0800

    feat: implement reconciliation onchain

---
+++ scripts/recon_integration_test.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+
+set -ex
+
+cd ./build
+if [ $? -ne 0 ]; then
+  echo "path build does not exists"
+  exit 1
+fi
+
+cli_home="./testnodecli"
+home="./testnoded"
+chain_id="bnbchain-1000"
+
+keys_operation_words="bnb"
+chain_operation_words="Committed"
+
+function prepare_node() {
+  cp -f ../networks/demo/*.exp .
+
+  rm -rf ${cli_home}
+  rm -rf ${home}
+  mkdir ${cli_home}
+  mkdir ${home}
+
+  secret=$(./bnbchaind init --moniker testnode --home ${home} --home-client ${cli_home} --chain-id ${chain_id} | grep secret | grep -o ":.*" | grep -o "\".*" | sed "s/\"//g")
+
+  $(cd "./${home}/config" && sed -i -e "s/BEP12Height = 9223372036854775807/BEP12Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP3Height = 9223372036854775807/BEP3Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/timeout_commit = \"1s\"/timeout_commit = \"500ms\"/g" config.toml)
+  $(cd "./${home}/config" && sed -i -e "s/log_level = \"main\:info,state\:info,\*\:error\"/log_level = \"*\:debug\"/g" config.toml)
+  $(cd "./${home}/config" && sed -i -e "s/\"min_self_delegation\": \"1000000000000\"/\"min_self_delegation\": \"10000000000\"/g" genesis.json)
+  $(cd "./${home}/config" && sed -i -e "s/BEP3Height = 9223372036854775807/BEP3Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP8Height = 9223372036854775807/BEP8Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP67Height = 9223372036854775807/BEP67Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP82Height = 9223372036854775807/BEP82Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP84Height = 9223372036854775807/BEP84Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP87Height = 9223372036854775807/BEP87Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/FixFailAckPackageHeight = 9223372036854775807/FixFailAckPackageHeight = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/EnableAccountScriptsForCrossChainTransferHeight = 9223372036854775807/EnableAccountScriptsForCrossChainTransferHeight = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP70Height = 9223372036854775807/BEP70Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP128Height = 9223372036854775807/BEP128Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP151Height = 9223372036854775807/BEP151Height = 1/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP153Height = 9223372036854775807/BEP153Height = 2/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP159Height = 9223372036854775807/BEP159Height = 3/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/BEP159Phase2Height = 9223372036854775807/BEP159Phase2Height = 11/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/LimitConsAddrUpdateIntervalHeight = 9223372036854775807/LimitConsAddrUpdateIntervalHeight = 11/g" app.toml)
+  $(cd "./${home}/config" && sed -i -e "s/breatheBlockInterval = 0/breatheBlockInterval = 5/g" app.toml)
+
+  # stop and start node
+  ps -ef | grep bnbchaind | grep testnoded | awk '{print $2}' | xargs kill -9
+  ./bnbchaind start --home ${home} >./testnoded/node.log 2>&1 &
+
+  echo ${secret}
+}
+
+function apply_upgrade() {
+  $(cd "./${home}/config" && sed -i -e "s/SecurityEnhancementHeight = 9223372036854775807/SecurityEnhancementHeight = 15/g" app.toml)
+
+  # stop and start node
+  ps -ef | grep bnbchaind | grep testnoded | awk '{print $2}' | xargs kill -9
+  ./bnbchaind start --home ${home} >./testnoded/node.log 2>&1 &
+}
+
+function exit_test() {
+  # stop node
+  ps -ef | grep bnbchaind | grep testnoded | awk '{print $2}' | xargs kill -9
+  exit $1
+}
+
+function check_operation() {
+  echo "$2"
+
+  echo "$2" | grep -q $3
+  if [ $? -ne 0 ]; then
+    echo "Checking $1 Failed"
+    exit_test 1
+  fi
+}
+
+secret=$(prepare_node)
+result=$(expect ./recover.exp "${secret}" "alice" true)
+check_operation "Recover Key" "${result}" "${keys_operation_words}"
+
+bob_secret="bottom quick strong ranch section decide pepper broken oven demand coin run jacket curious business achieve mule bamboo remain vote kid rigid bench rubber"
+result=$(expect ./add_key.exp "${bob_secret}" "bob")
+check_operation "Add Key" "${result}" "${keys_operation_words}"
+
+alice_addr=$(./bnbcli keys list --home ${cli_home} | grep alice | grep -o "bnb1[0-9a-zA-Z]*")
+bob_addr=$(./bnbcli keys list --home ${cli_home} | grep bob | grep -o "bnb1[0-9a-zA-Z]*")
+
+# wait for the chain
+sleep 10
+
+# send
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr})
+check_operation "Send Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# issue token
+result=$(expect ./issue.exp BTC Bitcoin 1000000000000000 true bob ${chain_id} ${cli_home})
+btc_symbol=$(echo "${result}" | tail -n 1 | grep -o "BTC-[0-9A-Z]*")
+check_operation "Issue Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# issue token
+result=$(expect ./issue.exp ETH Ethereum 1000000000000000 true bob ${chain_id} ${cli_home})
+eth_symbol=$(echo "${result}" | tail -n 1 | grep -o "ETH-[0-9A-Z]*")
+check_operation "Issue Token" "${result}" "${chain_operation_words}"
+
+apply_upgrade
+sleep 10 # wait for upgrade height
+
+# issue token failed, due to upgrade
+result=$(expect ./issue.exp USDT USDT 1000000000000000 true bob ${chain_id} ${cli_home})
+check_operation "Mint Token" "${result}" "ERROR:"
+
+sleep 1
+# send
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr})
+check_operation "Send Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# multi send
+echo ${bob_addr}
+result=$(expect ./multi_send.exp ${cli_home} alice ${chain_id} "[{\"to\":\"${bob_addr}\",\"amount\":\"100000000000000:BNB\"},{\"to\":\"${alice_addr}\",\"amount\":\"100000000000000:BNB\"}]")
+check_operation "Multi Send Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# mint token
+result=$(expect ./mint.exp ${btc_symbol} 1000000000000000 bob ${chain_id} ${cli_home})
+check_operation "Mint Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# burn token
+result=$(expect ./mint.exp ${btc_symbol} 50000000 bob ${chain_id} ${cli_home})
+check_operation "Mint Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# propose list
+((expire_time = $(date '+%s') + 1000))
+lower_case_btc_symbol=$(echo ${btc_symbol} | tr 'A-Z' 'a-z')
+result=$(expect ./propose_list.exp ${chain_id} alice 200000000000:BNB ${lower_case_btc_symbol} bnb 100000000 "list BTC/BNB" "list BTC/BNB" ${cli_home} ${expire_time} 5)
+check_operation "Propose list" "${result}" "${chain_operation_words}"
+
+sleep 2
+# vote for propose
+result=$(expect ./vote.exp alice ${chain_id} 1 Yes ${cli_home})
+check_operation "Vote" "${result}" "${chain_operation_words}"
+
+sleep 1
+## query account balance
+result=$(./bnbcli account $bob_addr --trust-node)
+balance1=$(echo "${result}" | jq -r '.value.base.coins[0].amount')
+
+sleep 1
+# set an account flag which isn't bounded to transfer memo checker script
+result=$(expect ./set_account_flags.exp 0x02 bob ${chain_id} ${cli_home})
+check_operation "Set account flags" "${result}" "${chain_operation_words}"
+
+sleep 1
+## query account balance
+result=$(./bnbcli account $bob_addr --trust-node)
+balance2=$(echo "${result}" | jq -r '.value.base.coins[0].amount')
+
+check_operation "Check fee deduction for set account flags transaction" "$(expr $balance2 - $balance1)" "100000000"
+
+sleep 1
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr})
+check_operation "Send Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr} "123456abcd")
+check_operation "Send Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# set an account flag which is bounded to transfer memo checker script
+result=$(expect ./set_account_flags.exp 0x01 bob ${chain_id} ${cli_home})
+check_operation "Set account flags" "${result}" "${chain_operation_words}"
+
+sleep 1
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr})
+check_operation "Send Token" "${result}" "ERROR"
+
+sleep 1
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr} "123456abcd")
+check_operation "Send Token" "${result}" "ERROR:"
+
+sleep 1
+result=$(expect ./send.exp ${cli_home} alice ${chain_id} "100000000000000:BNB" ${bob_addr} "1234567890")
+check_operation "Send Token" "${result}" "${chain_operation_words}"
+
+sleep 1
+# Create an atomic swap
+result=$(expect ./HTLT-cross-chain.exp 2000 "100000000:BNB" "100000000:BNB" $bob_addr 0xf2fbB6C41271064613D6f44C7EE9A6c471Ec9B25 alice ${chain_id} ${cli_home})
+check_operation "Create an atomic swap" "${result}" "${chain_operation_words}"
+randomNumber=$(sed 's/Random number: //g' <<<$(echo "${result}" | grep -o "Random number: [0-9a-z]*"))
+timestamp=$(sed 's/Timestamp: //g' <<<$(echo "${result}" | grep -o "Timestamp: [0-9]*"))
+randomNumberHash=$(sed 's/Random number hash: //g' <<<$(echo "${result}" | grep -o "Random number hash: [0-9a-z]*"))
+swapID=$(sed 's/swapID: //g' <<<$(echo "${result}" | tail -n 1 | grep -o "swapID: [0-9a-z]*"))
+sleep 1
+
+atomicSwap=$(./bnbcli token query-swap --swap-id ${swapID} --trust-node)
+swapFrom=$(echo "${atomicSwap}" | jq -r '.from')
+check_operation "Check swap creator address" $swapFrom $alice_addr
+swapTo=$(echo "${atomicSwap}" | jq -r '.to')
+check_operation "swap recipient address" $swapTo $bob_addr
+
+result=$(./bnbcli account bnb1wxeplyw7x8aahy93w96yhwm7xcq3ke4f8ge93u --trust-node)
+swapDeadAddrBalance=$(echo "${result}" | jq -r '.value.base.coins[0].amount')
+check_operation "the balance of swap dead address" $swapDeadAddrBalance "100000000"
+
+result=$(./bnbcli account $bob_addr --trust-node)
+balanceBobBeforeClaim=$(echo "${result}" | jq -r '.value.base.coins[0].amount')
+
+# Claim an atomic swap
+result=$(expect ./claim.exp ${swapID} $randomNumber alice ${chain_id} ${cli_home})
+check_operation "claim an atomic swap" "${result}" "${chain_operation_words}"
+
+sleep 1
+
+result=$(./bnbcli account $bob_addr --trust-node)
+balanceBobAfterClaim=$(echo "${result}" | jq -r '.value.base.coins[0].amount')
+check_operation "Bob balance after claim swap" "$(expr $balanceBobAfterClaim - $balanceBobBeforeClaim)" "100000000"
+
+# Create an atomic swap
+result=$(expect ./HTLT-cross-chain.exp 2000 "100000000:BNB" "100000000:BNB" $alice_addr 0xf2fbB6C41271064613D6f44C7EE9A6c471Ec9B25 bob ${chain_id} ${cli_home})
+check_operation "Create an atomic swap" "${result}" "${chain_operation_words}"
+swapID=$(sed 's/swapID: //g' <<<$(echo "${result}" | tail -n 1 | grep -o "swapID: [0-9a-z]*"))
+
+sleep 1
+
+# Refund an atomic swap
+result=$(expect ./refund.exp ${swapID} alice ${chain_id} ${cli_home})
+check_operation "refund an atomic swap which is still not expired" "${result}" "ERROR"
+
+sleep 1
+
+result=$(./bnbcli account bnb1wxeplyw7x8aahy93w96yhwm7xcq3ke4f8ge93u --trust-node)
+swapDeadAddrBalance=$(echo "${result}" | jq -r '.value.base.coins[0].amount')
+check_operation "the balance of swap dead address" $swapDeadAddrBalance "100000000"
+
+sleep 1
+# Create a single chain atomic swap
+result=$(expect ./HTLT-single-chain.exp 2000 "100000000:BNB" "10000:${eth_symbol}" $bob_addr alice ${chain_id} ${cli_home})
+check_operation "Create a single chain atomic swap" "${result}" "${chain_operation_words}"
+randomNumber=$(sed 's/Random number: //g' <<<$(echo "${result}" | grep -o "Random number: [0-9a-z]*"))
+timestamp=$(sed 's/Timestamp: //g' <<<$(echo "${result}" | grep -o "Timestamp: [0-9]*"))
+randomNumberHash=$(sed 's/Random number hash: //g' <<<$(echo "${result}" | grep -o "Random number hash: [0-9a-z]*"))
+swapID=$(sed 's/swapID: //g' <<<$(echo "${result}" | tail -n 1 | grep -o "swapID: [0-9a-z]*"))
+sleep 1
+
+# Deposit to a single chain atomic swap
+result=$(expect ./deposit.exp ${swapID} "10000:${eth_symbol}" bob ${chain_id} ${cli_home})
+check_operation "Deposit to a single chain atomic swap" "${result}" "${chain_operation_words}"
+
+sleep 1
+# claim a single chain atomic swap
+result=$(expect ./claim.exp ${swapID} ${randomNumber} alice ${chain_id} ${cli_home})
+check_operation "claim a single chain atomic swap" "${result}" "${chain_operation_words}"
+
+sleep 1
+# Deposit to a single chain atomic swap
+result=$(expect ./deposit.exp ${swapID} "10000:${eth_symbol}" bob ${chain_id} ${cli_home})
+check_operation "Deposit to a closed single chain atomic swap" "${result}" "ERROR"
+
+sleep 1
+# Create a single chain atomic swa
+result=$(expect ./HTLT-single-chain.exp 360 "100000000:BNB" "10000:${eth_symbol}" $bob_addr alice ${chain_id} ${cli_home})
+check_operation "Create a single chain atomic swap" "${result}" "${chain_operation_words}"
+randomNumber=$(sed 's/Random number: //g' <<<$(echo "${result}" | grep -o "Random number: [0-9a-z]*"))
+timestamp=$(sed 's/Timestamp: //g' <<<$(echo "${result}" | grep -o "Timestamp: [0-9]*"))
+randomNumberHash=$(sed 's/Random number hash: //g' <<<$(echo "${result}" | grep -o "Random number hash: [0-9a-z]*"))
+swapID=$(sed 's/swapID: //g' <<<$(echo "${result}" | tail -n 1 | grep -o "swapID: [0-9a-z]*"))
+
+sleep 1
+# Deposit to a single chain atomic swap
+result=$(expect ./deposit.exp ${swapID} "10000:${eth_symbol}" bob ${chain_id} ${cli_home})
+check_operation "Deposit to a single chain atomic swap" "${result}" "${chain_operation_words}"
+
+sleep 1
+# Deposit to a single chain atomic swap
+result=$(expect ./deposit.exp ${swapID} "10000:${eth_symbol}" bob ${chain_id} ${cli_home})
+check_operation "Deposit to a deposited single chain atomic swap" "${result}" "ERROR"
+
+exit_test 0
