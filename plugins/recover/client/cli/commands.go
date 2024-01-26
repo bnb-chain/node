a3543ab4b8e4af9fc825333da68af1ada9d5777b
dylanhuang <j75689@gmail.com>
2024-01-26 14:17:16 +0800

    chore: refine codes (#1007)

--- plugins/recover/client/cli/commands.go
+++ plugins/recover/client/cli/commands.go
@@ -7,16 +7,25 @@ import (
 )
 
 func AddCommands(cmd *cobra.Command, cdc *codec.Codec) {
-	airdropCmd := &cobra.Command{
+	recoverCmd := &cobra.Command{
 		Use:   "recover",
 		Short: "recover commands",
+		Long: `recover commands is a tool for users to sign a request to recover their tokens from Binance Chain to Binance Smart Chain
+		# For example:
+		bnbcli recover sign-token-recover-request \
+		 --amount 19999999000000000 \
+		 --token-symbol BNB \
+		 --recipient 0x5b38da6a701c568545dcfcb03fcb875f56beddc4 \
+		 --from user1 \
+		 --chain-id Binance-Chain-Tigris
+		`,
 	}
 
-	airdropCmd.AddCommand(
+	recoverCmd.AddCommand(
 		client.PostCommands(
 			SignTokenRecoverRequestCmd(cdc),
 		)...,
 	)
 
-	cmd.AddCommand(airdropCmd)
+	cmd.AddCommand(recoverCmd)
 }
