4837b6f650d9ae7afea607af89c8661305f4db08
j75689 <j75689@gmail.com>
2024-01-26 14:06:07 +0800

    chore: refine codes

--- plugins/migrate/client/cli/commands.go
+++ plugins/migrate/client/cli/commands.go
@@ -10,6 +10,12 @@ func AddCommands(cmd *cobra.Command, cdc *codec.Codec) {
 	ownerShipCmd := &cobra.Command{
 		Use:   "validator-ownership",
 		Short: "validator-ownership commands",
+		Long: `validator-ownership commands is a tool to help BSC validator operator create a mapping signature to New Validator on BSC
+		# For example:
+		bnbcli validator-ownership sign-validator-ownership \
+		 --bsc-operator-address 0x45737bAf95D995a963ab3a7c9AC66fC7A63ad76E \
+		 --from bsc-operator \
+		 --chain-id Binance-Chain-Tigris`,
 	}
 
 	ownerShipCmd.AddCommand(
